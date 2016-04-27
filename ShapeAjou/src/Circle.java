ea3ed433f27c7ebf03fee7d6fe51b8df551da9d0
Hojeong <will9102002@naver.com>
2016-04-27 16:35:53 +0900

    iiiii

---
+++ ShapeAjou/src/Circle.java
@@ -0,0 +1,36 @@
+public class   Circle  extends Shape{
+
+private double radius;
+
+
+
+	public double getRadius() {
+	return radius;
+}
+
+
+
+public void setRadius(double radius) {
+	this.radius = radius;
+}
+
+public double calculateArea()
+{ 
+	return radius*radius;
+}
+public double calculatePerimeter()
+{
+	return;
+}
+public void printinfo()
+{
+	
+}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
